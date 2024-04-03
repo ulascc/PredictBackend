20f6ca145d03391a1aeb1ad0bc008a12f2c094eb
ulascc <ulas.can2424@gmail.com>
2024-04-03 11:48:48 +0300

    token feature added

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ from database_operation import get_db
 from models import User, UserCreate, UserLogin
 from database import SessionLocal, engine, Base
 from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime, timedelta
+import jwt
+import secrets
+
+# Secret key for signing JWT
+SECRET_KEY = secrets.token_urlsafe(32)
+# Algorithm to sign JWT
+ALGORITHM = "HS256"
+
+# Token expiration time in minutes
+ACCESS_TOKEN_EXPIRE_MINUTES = 0.1
 
 Base.metadata.create_all(bind=engine)
 
@@ -17,6 +28,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Function to create access token
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    # Set token expiration time
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    # Encode token
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# Function to verify token
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except jwt.JWTError:
+        return None
 
 @app.post("/register/")
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
@@ -39,10 +67,19 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
 
 @app.post("/login/")
 def login_user(user_login: UserLogin, db: Session = Depends(get_db)):
-
     user = db.query(User).filter(User.email == user_login.email).first()
 
     if not user or user.password != user_login.password:
         raise HTTPException(status_code=401, detail="Geçersiz e-posta veya şifre")
     
-    return {"status": 200, "message": "Başarıyla giriş yapıldı"}
+    # If user is authenticated, create and return access token
+    access_token = create_access_token(data={"email": user.email})
+    return {"access_token": access_token, "token_type": "bearer", "status": 200, "message": "Başarıyla giriş yapıldı"}
+
+
+# Example protected endpoint that requires authentication
+@app.get("/protected/")
+def protected_route(token: str = Depends(decode_access_token)):
+    if not token:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    return {"message": "You are accessing a protected route."}
